6097d8784752ac9cc4f059138ee7cea64dfd93e5
joseph-rato <joseph.rato@gmail.com>
2018-08-23 22:37:09 -0700

    Created basic Stack with basic methods

---
+++ W1D5/DIY_ADTs.rb
@@ -0,0 +1,23 @@
+
+class Stack
+  attr_reader :stack
+  def initialize
+    @stack = []
+  end
+
+  def push(el)
+    stack.push(el)
+  end
+
+  def pop
+    stack.pop
+  end
+
+  def peek
+    stack[-1]
+  end
+
+end
+
+private
+attr_reader :stack
