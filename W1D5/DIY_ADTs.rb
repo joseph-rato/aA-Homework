bd936ee4cf51c03a6c432b71b0b50d743f7a87d4
joseph-rato <joseph.rato@gmail.com>
2018-08-23 22:53:55 -0700

    made Map class and set method

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -38,3 +38,33 @@ class Queue
   end
 
 end
+
+class Map
+  attr_reader :map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    return @map << [key, value] if @map.empty?
+    @map.each do |array_pairs|
+      if array_pairs[0] == key
+        array_pairs[-1] = value
+      else
+        @map << [key, value]
+        p "no"
+      end
+    end
+  end
+
+  def get(key)
+  end
+
+  def delete(key)
+  end
+
+  def show
+    p "#{}"
+  end
+
+end
