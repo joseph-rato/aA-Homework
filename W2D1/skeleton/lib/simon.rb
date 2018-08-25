ef905b43acbaa6a567577c24f10dc12590e88b3b
joseph-rato <joseph.rato@gmail.com>
2018-08-25 11:33:16 -0700

    successfully pass specs

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -11,35 +11,77 @@ class Simon
   end
 
   def play
-
+    until game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    @sequence_length += 1
+    # debugger
+    input = require_sequence
+    round_success_message
+    # debugger
+    # if check_seq_to_input(input) == false
+    #   return @game_over = true
+    # end
+    # round_success_message
   end
 
   def show_sequence
+    # debugger
+    add_random_color
+    seq.each do |color|
+      p color
+      sleep(3)
+      system("clear")
+    end
+  end
 
+  def check_seq_to_input(input)
+    if @seq == input
+      return true
+    end
+    false
   end
 
   def require_sequence
-
+    p "please list the colors the same way they were shown to you"
+    p "Ie. red blue green"
+    user_input = $stdin.chomp.split(" ")
+    if user_input.all? {|color_input| COLORS.include?(color_input)} == true
+      return user_input
+    end
+    p "only put in these colors #{COLORS}"
+    require_sequence
   end
 
   def add_random_color
-    colors = %w(red blue yellow green)
-    @seq << colors.sample
+    # colors = %w(red blue yellow green)
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    p "Nice you gottem all! Can you do another one?"
+    p "Anotha one!"
   end
 
   def game_over_message
-
+    p "too bad you forgot the sequence! But you remembered #{sequence_length - 1}"
+    p "try and get #{sequence_length} or more this time!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+    p "do you want to play again? (y/n)"
+    input = $stdin#.chomp
+    if input == "y"
+      play
+    end
   end
 end
