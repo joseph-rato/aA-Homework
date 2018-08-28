632df8df77d32b22f8bebc03b23a8282b68fd571
joseph-rato <joseph.rato@gmail.com>
2018-08-28 06:47:33 -0700

    finish initialize, place stones and valid move only start make_move

---
+++ W2D2/skeleton/lib/board.rb
@@ -0,0 +1,67 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @player1 = name1#Player.new(name1, (0..5).to_a)
+    @player2 = name2#Player.new(name2, (6..13).to_a)
+    @cups = Array.new(14) {[]}
+    place_stones
+  end
+
+  def place_stones
+    @cups.each_index do |i|
+      @cups[i] = [:stone, :stone, :stone, :stone] unless i == 6 || i == 13
+    end
+    # helper method to #initialize every non-store cup with four stones each
+  end
+
+  class EmptyCup < StandardError
+    def message
+      "Starting cup is empty"
+    end
+  end
+  class NonExistantCup < StandardError
+    def message
+      "Invalid starting cup"
+    end
+  end
+
+  def valid_move?(start_pos)
+    unless start_pos.between?(0, 13)
+      raise NonExistantCup
+    end
+    if @cups[start_pos].empty?
+      raise EmptyCup
+    end
+
+  end
+
+  def make_move(start_pos, current_player_name)
+    turn_stones = []
+    stones = @cups[start_pos].shift(cups[start_pos].size)
+    turn_stones += stones
+    p turn_stones
+    @cups[(start_pos+1)...turn_stones.length].each do |cup|
+      cup += [turn_stones.shift]
+    end
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[0..5].all? {|cup| cup.empty?} || @cups[7..12].all? {|cup| cup.empty?}
+  end
+
+  def winner
+  end
+end
