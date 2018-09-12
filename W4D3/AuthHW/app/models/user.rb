10c2503ee827bb1b44647c32cff786335a168e7b
joseph-rato <joseph.rato@gmail.com>
2018-09-12 08:54:41 -0700

    finish the first authoHW

--- W4D3/AuthHW/app/models/user.rb
+++ W4D3/AuthHW/app/models/user.rb
@@ -20,9 +20,19 @@ class User < ApplicationRecord
     SecureRandom::urlsafe_base64(16)
   end
 
-  # def res
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
 
   def ensure_session_token
     self.session_token || self.class.generate_session_tolken
+  end
+
+  def self.find_by_credentials
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
   end
 end
